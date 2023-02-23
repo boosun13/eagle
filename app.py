7517abfa7f1c2258000987f00e530e202040c069
boosun13 <double0two22@gmail.com>
2023-02-23 21:02:29 +0900

    Fix method name and try raise method

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/check', methods=['GET'])
-def get():
-    return 'ok'
+
+@app.route("/check", methods=["GET"])
+def check():
+    return "ok"
+
+
+@app.route("/error_check", methods=["GET"])
+def error_check():
+    raise "ng"
